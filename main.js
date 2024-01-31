81e8485a09589244e3fed0008e3484fb9a5f2aec
Yilin <yilin.zh135@gmail.com>
2024-01-31 20:27:59 +0100

    add a URL

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hola")
+console.log("hola");
+
+const URL='https://api.thecatapi.com/v1/images/search';
+
+fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        data[0].url
+    })
