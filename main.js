52e21ce61c7f4cfec75f505a610af2a1e213d920
Yilin <yilin.zh135@gmail.com>
2024-01-31 21:09:55 +0100

    Async y await button

--- main.js
+++ main.js
@@ -2,8 +2,21 @@ console.log("hola");
 
 const URL='https://api.thecatapi.com/v1/images/search';
 
-fetch(URL)
+/*fetch(URL)
     .then(res => res.json())
     .then(data => {
-        data[0].url
+        const img =document.querySelector('img');
+        img.src = data[0].url;
     })
+*/
+const img =document.querySelector('img');
+
+const cambiarImagen = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+    img.src = data[0].url;
+}
+
+let boton = document.getElementById('btn');
+cambiarImagen()
+boton.addEventListener('click',cambiarImagen)
