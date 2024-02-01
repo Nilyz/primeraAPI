d664f1abe957b59c4bd949ff67d985d1ddb850aa
Yilin <yilin.zh135@gmail.com>
2024-02-01 19:49:35 +0100

    Fetch with errror
    no funciona xd

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 
-const URL='https://api.thecatapi.com/v1/images/search?limit=10&page=0&api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9';
+const URL_random='https://api.thecatapi.com/v1/images/search?limit=10&page=0&api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9';
+const URL_favoritos='https://api.thecatapi.com/v1/favourites?limit=10&page=0';
+
+const spanError= document.getElementById('error');
 
 /*fetch(URL)
     .then(res => res.json())
@@ -11,19 +14,41 @@ const URL='https://api.thecatapi.com/v1/images/search?limit=10&page=0&api_key=li
 */
 
 
-const cambiarImagen = async () => {
-    const res = await fetch(URL);
+const cambiarTresImagenRandoms = async () => {
+    const res = await fetch(URL_random);
     const data = await res.json();
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
-    console.log(data)
+ 
+
+    console.log('randoms',data);
+    if(res.status!==200){
+        spanError.innerHTML = 'Hubo un error' + res.status;
+    }else{
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
+}
+
+const  favoritosMichis = async () =>{
+    const res = await fetch(URL_favoritos);
+    const data = await res.json();
+ 
+    if(res.status!==200){
+        spanError.innerHTML = 'Hubo un error' + res.status;
+    }else{
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
+
 }
 
 let boton = document.getElementById('btn');
-cambiarImagen()
-boton.addEventListener('click',cambiarImagen)
+
+
+cambiarTresImagenRandoms();
+favoritosMichis();
+
+boton.addEventListener('click',cambiarTresImagenRandoms);
 
 let img1 = document.getElementById('img1');
 let img2 = document.getElementById('img2');
-let img3 = document.getElementById('img3');
+
