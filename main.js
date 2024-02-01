c5aaf47cdfd0fc60431c1357b4fa49bde506dbc9
Yilin <yilin.zh135@gmail.com>
2024-02-01 22:30:53 +0100

    final(en verdad no xd)

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const URL_random =
 const URL_favoritos =
     "https://api.thecatapi.com/v1/favourites?api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9";
 
+const URL_eliminar = (id)=>`https://api.thecatapi.com/v1/favourites/${id}?api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9`
 const spanError = document.getElementById("error");
 
 /*fetch(URL)
@@ -20,17 +21,18 @@ const cambiarTresImagenRandoms = async () => {
     }
     const data = await res.json();
     console.log("randoms", data);
+    
+    let img1 = document.getElementById("img1");
+    let img2 = document.getElementById("img2");
     img1.src = data[0].url;
     img2.src = data[1].url;
 
-    let img1 = document.getElementById("img1");
-    let img2 = document.getElementById("img2");
-
     let btn_fav1 = document.getElementById("btn_fav1");
-    btn_fav1.addEventListener("click", guardarFavoritos);
+    btn_fav1.addEventListener("click",() => guardarFavoritos(data[0].id));
 
     let btn_fav2 = document.getElementById("btn_fav2");
-    btn_fav2.addEventListener("click", guardarFavoritos);
+    btn_fav2.addEventListener("click",() => guardarFavoritos(data[1].id));
+
 };
 
 const favoritosMichis = async () => {
@@ -42,8 +44,9 @@ const favoritosMichis = async () => {
     const data = await res.json();
 
     console.log("favoritos", data);
+    const section = document.getElementById("michisFav");
+    section.innerHTML='';
     data.forEach((michi) => {
-        const section = document.getElementById("michisFav");
         const article = document.createElement("article");
         const imagen = document.createElement("img");
         const btn = document.createElement("button");
@@ -55,6 +58,7 @@ const favoritosMichis = async () => {
         article.appendChild(imagen);
         article.appendChild(btn);
         section.appendChild(article);
+        btn.addEventListener('click',()=>eliminarFavoritos(michi.id));
     });
 };
 
@@ -72,9 +76,26 @@ const guardarFavoritos = async (id) => {
 
     if (res.status < 200 || res.status > 299) {
         spanError.innerHTML = "Hubo un error: " + res.status;
+        return null;
     }
-
+    console.log('michi guardado')
     console.log(res);
+    favoritosMichis();
+};
+
+const eliminarFavoritos = async (id) => {
+    const res = await fetch(URL_eliminar(id), {
+        method: "DELETE"  
+    });
+
+    if (res.status < 200 || res.status > 299) {
+        spanError.innerHTML = "Hubo un error: " + res.status;
+        return null;
+    }
+    console.log('michi eliminado');
+    console.log(res);
+    favoritosMichis();
+    
 };
 
 let boton = document.getElementById("btn");
