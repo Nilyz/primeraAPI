84d365a421520d4fe1fca1277141d3a316fbbbb8
Ángel Garcia Beltrán <angelurano00@gmail.com>
2024-02-01 20:04:19 +0100

    Fix error with print error response in fetch

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
-
 const URL_random='https://api.thecatapi.com/v1/images/search?limit=10&page=0&api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9';
-const URL_favoritos='https://api.thecatapi.com/v1/favourites?limit=10&page=0';
+const URL_favoritos='https://api.thecatapi.com/v1/favourites';
 
 const spanError= document.getElementById('error');
 
@@ -16,28 +14,26 @@ const spanError= document.getElementById('error');
 
 const cambiarTresImagenRandoms = async () => {
     const res = await fetch(URL_random);
-    const data = await res.json();
- 
-
-    console.log('randoms',data);
-    if(res.status!==200){
+    if(res.status<200 || res.status>299){
         spanError.innerHTML = 'Hubo un error' + res.status;
-    }else{
-        img1.src = data[0].url;
-        img2.src = data[1].url;
     }
+    const data = await res.json();
+    console.log('randoms',data);
+    img1.src = data[0].url;
+    img2.src = data[1].url;
 }
 
 const  favoritosMichis = async () =>{
     const res = await fetch(URL_favoritos);
-    const data = await res.json();
- 
-    if(res.status!==200){
-        spanError.innerHTML = 'Hubo un error' + res.status;
-    }else{
-        img1.src = data[0].url;
-        img2.src = data[1].url;
+    if(res.status<200 || res.status>299){
+        spanError.innerHTML = 'Hubo un error: ' + res.status;
     }
+    const data = await res.json();
+    console.log('favoritos', data);
+
+    /*if(res.status!==200){
+    }else{
+    } */
 
 }
 
