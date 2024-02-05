65746c862532f2bf72080b6260757851fd9776e4
Yilin <yilin.zh135@gmail.com>
2024-02-05 22:45:05 +0100

    add Axios

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const URL_random =
-    "https://api.thecatapi.com/v1/images/search?limit=10&page=0&api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9";
-const URL_favoritos =
-    "https://api.thecatapi.com/v1/favourites?api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9";
+const api= axios.create({
+    baseURL: 'https://api.thecatapi.com/v1/'
 
+})
+
+api.defaults.headers.common['X-API-KEY']= 'live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9'
+
+
+const URL_random =
+    "https://api.thecatapi.com/v1/images/search?limit=10&page=0";
+const URL_favoritos =
+    "https://api.thecatapi.com/v1/favourites";
+const URL_imgUpload =
+    "https://api.thecatapi.com/v1/images/upload"
 const URL_eliminar = (id)=>`https://api.thecatapi.com/v1/favourites/${id}?api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9`
 const spanError = document.getElementById("error");
 
@@ -36,7 +45,12 @@ const cambiarTresImagenRandoms = async () => {
 };
 
 const favoritosMichis = async () => {
-    const res = await fetch(URL_favoritos);
+    const res = await fetch(URL_favoritos, {
+        method: 'GET',
+        headers:{
+            'X-API-KEY': 'live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9'
+        }
+    })
     if (res.status < 200 || res.status > 299) {
         spanError.innerHTML = "Hubo un error: " + res.status;
         return null;
@@ -63,43 +77,86 @@ const favoritosMichis = async () => {
 };
 
 const guardarFavoritos = async (id) => {
-    console.log("entrando a favoritos");
+    const {data, status} = await api.post('/favourites',{
+        image_id: id,
+    });
+
+    /*console.log("entrando a favoritos");
     const res = await fetch(URL_favoritos, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            'X-API-KEY': 'live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9'
         },
         body: JSON.stringify({
             image_id: id,
         }),
     });
-
-    if (res.status < 200 || res.status > 299) {
+    */
+    if (status < 200 || status > 299) {
         spanError.innerHTML = "Hubo un error: " + res.status;
         return null;
     }
-    console.log('michi guardado')
-    console.log(res);
+
+    //console.log('michi guardado')
+    //console.log(res);
     favoritosMichis();
 };
 
 const eliminarFavoritos = async (id) => {
-    const res = await fetch(URL_eliminar(id), {
-        method: "DELETE"  
+
+   /* const res = await fetch(URL_eliminar(id), {
+        method: "DELETE",  
+        headers: {
+            'X-API-KEY': 'live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9'
+        }
     });
 
     if (res.status < 200 || res.status > 299) {
         spanError.innerHTML = "Hubo un error: " + res.status;
         return null;
-    }
-    console.log('michi eliminado');
-    console.log(res);
+    }*/
+    //console.log('michi eliminado');
+
+    const {data, status} = await api.delete(`/favourites/${id}?api_key=live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9`,{
+        image_id: id,
+    });
+
+ 
     favoritosMichis();
     
 };
 
+
+
+
+const subirFoto=async ()=>{
+
+    const form=document.getElementById('uploadingForm')
+    const formData = new FormData(form);
+    //console.log(formData.get('file'))
+    const res = await fetch(URL_imgUpload, {
+        method: "POST",
+        headers: {
+            //'Content-Type': 'multipart/form-data',
+            'X-API-KEY': 'live_5v8rcjNV9f0gPMwDNBaqrKm6rNTyP2fwj7ylWCxxCvJwCMdTrQF9df4LiJTupue9'
+        },
+        body:formData
+    });
+    const data = await res.json();
+    //console.log(data);
+    guardarFavoritos(data.id);
+}
+
+
+
 let boton = document.getElementById("btn");
 boton.addEventListener("click", cambiarTresImagenRandoms);
+
+
+
+let botonSubirFoto =document.getElementById('botonSubirFoto');
+botonSubirFoto.addEventListener('click',subirFoto);
 
 cambiarTresImagenRandoms();
 favoritosMichis();
